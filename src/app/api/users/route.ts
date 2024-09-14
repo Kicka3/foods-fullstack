efb3ba447e753aac6fa99bf44c5ea43cfa26dbd8
Kirill <kirill_ozornin@mail.ru>
2024-09-14 21:04:55 +0500

    task-6 fix: "route POST/GET"

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,23 @@
 import { prisma } from "../../../../prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-  const users = await prisma.users.findMany();
+  const users = await prisma.user.findMany();
 
   return NextResponse.json(users);
 }
 
-export async function POST() {}
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+  }
+}
