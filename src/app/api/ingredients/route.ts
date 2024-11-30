27adc71208c49c85597af1c8bdb71055a31e06a0
Kirill <kirill_ozornin@mail.ru>
2024-11-30 16:53:10 +0500

    task-8 feat: seeding

---
+++ src/app/api/ingredients/route.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const ingredients = await prisma.ingredient.findMany();
+  return NextResponse.json(ingredients);
+}
+
+//5.18
